4bc1bc804dc5570f9bbf105250835292abb0e917
Fawaz Sammani <sammanifawaz@gmail.com>
2020-04-10 12:50:21 +0800

    Update mog_lstm.py

--- mog_lstm.py
+++ mog_lstm.py
@@ -6,27 +6,16 @@ class MogrifierLSTMCell(nn.Module):
 
     def __init__(self, input_size, hidden_size, mogrify_steps):
         super(MogrifierLSTMCell, self).__init__()
-        self.hidden_size = hidden_size
-        self.input_size = input_size
-        self.mogrify_steps = mogrify_steps
-        self.x2h = nn.Linear(input_size, 4 * hidden_size)
-        self.h2h = nn.Linear(hidden_size, 4 * hidden_size)
         
+        self.mogrify_steps = mogrify_steps
+        self.lstm = nn.LSTMCell(input_size, hidden_size)
         self.mogrifier_list = nn.ModuleList([nn.Linear(hidden_size, input_size)])  # start with q
         for i in range(1, mogrify_steps):
             if i % 2 == 0:
                 self.mogrifier_list.extend([nn.Linear(hidden_size, input_size)])  # q
             else:
                 self.mogrifier_list.extend([nn.Linear(input_size, hidden_size)])  # r
-        
-        self.tanh = nn.Tanh()
-        self.init_parameters()
-        
-    def init_parameters(self):
-        std = 1.0 / math.sqrt(self.hidden_size)
-        for p in self.parameters():
-            p.data.uniform_(-std, std)
-            
+   
     def mogrify(self, x, h):
         for i in range(self.mogrify_steps):
             if (i+1) % 2 == 0: 
@@ -36,19 +25,8 @@ class MogrifierLSTMCell(nn.Module):
         return x, h
 
     def forward(self, x, states):
-        """
-        inp shape: (batch_size, input_size)
-        each of states shape: (batch_size, hidden_size)
-        """
+        
         ht, ct = states
-        x, ht = self.mogrify(x,ht)   # Note: This should be called every timestep
-        gates = self.x2h(x) + self.h2h(ht)    # (batch_size, 4 * hidden_size)
-        in_gate, forget_gate, new_memory, out_gate = gates.chunk(4, 1)
-        in_gate = torch.sigmoid(in_gate)
-        forget_gate = torch.sigmoid(forget_gate)
-        out_gate = torch.sigmoid(out_gate)
-        new_memory = self.tanh(new_memory)
-        c_new = (forget_gate * ct) + (in_gate * new_memory)
-        h_new = out_gate * self.tanh(c_new)
-
-        return h_new, c_new
+        x, ht = self.mogrify(x, ht)   # Note: This should be called every timestep
+        ht, ct = self.lstm(x, (ht, ct))
+        return ht, ct
